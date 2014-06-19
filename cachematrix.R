8422775dd9347f45c4f30b27162d22d101ba25ca
bergwc55b <berg55.rick@gmail.com>
2014-06-18 20:30:56 -0400

    First put a code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+temp<-matrix()
 
-## Write a short comment describing this function
+## Function that inverses the matrix and caches the result.  
+###If no matrix is provided as input then it returns the cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    if ( is.matrix(x)) {
+        im<-Solve(x)
+    }
+    im
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of a matrix and save it for later use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    if ( temp != x ) {
+        temp<-x
+        makeCacheMatrix(x)
+    } else {
+        makeCacheMatrix()
+    }
 }
